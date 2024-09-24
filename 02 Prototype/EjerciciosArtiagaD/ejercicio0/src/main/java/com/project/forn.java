665243c3a4adf9d64b72e3af1ac0f2e91c61e909
Daniel Artiaga Perez <dartiagaperez.cf@iesesteveterradas.cat>
2024-09-24 20:17:20 +0200

    commint

---
+++ 02 Prototype/EjerciciosArtiagaD/ejercicio0/src/main/java/com/project/forn.java
@@ -0,0 +1,12 @@
+package main.java.com.project;
+
+public class forn extends electrodomestic {
+    
+    public forn(String nom, String color, String marca, String eficiencia, float preu) {
+        super(nom, color, marca, eficiencia, preu);
+        //TODO Auto-generated constructor stub
+    }
+    private int temperatura;
+    private boolean autoneteja;
+
+}
---
+++ 02 Prototype/EjerciciosArtiagaD/ejercicio0/src/main/java/com/project/rentadora.java
@@ -0,0 +1,12 @@
+package main.java.com.project;
+
+public class rentadora extends electrodomestic{
+    
+    public rentadora(String nom, String color, String marca, String eficiencia, float preu, int soroll, int revolucions) {
+        super(nom, color, marca, eficiencia, preu, soroll, revolucions);
+        //TODO Auto-generated constructor stub
+    }
+    private int soroll;
+    private int revolucions;
+
+}
